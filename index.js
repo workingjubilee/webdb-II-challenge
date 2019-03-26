c66d95a860cabdeb1a64ca6e4d2b3bc8ab9f0818
workingjubilee <workingjubilee@gmail.com>
2019-03-26 15:21:03 -0700

    Knex init

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/', async (req,res) => {
+  try { 
+    let reply = await db.from();
+    res.status(200).json(reply);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
